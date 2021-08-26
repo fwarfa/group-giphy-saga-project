2ed7bc102b8d7bae331f74afc28482e6bcb12af2
Curtis Kettler <curtiskettler@gmail.com>
2021-08-26 14:04:34 -0500

    GET working with db

--- database.sql
+++ database.sql
@@ -1,9 +1,3 @@
-CREATE DATABASE "giphy_search_favorites";
-
--- You'll need a table for storing each giphy image favorite
--- Each favorite image can be assigned 1 of the following categories as a Foreign Key
-
--- Category table
 CREATE TABLE "category" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR (100) NOT NULL
@@ -13,7 +7,6 @@ CREATE TABLE "category" (
 INSERT INTO "category" ("name")
 VALUES ('funny'), ('cohort'), ('cartoon'), ('nsfw'), ('meme');
 
-
 CREATE TABLE "favorites" (
     "id" SERIAL PRIMARY KEY,
     "url" VARCHAR (100) NOT NULL,
@@ -21,3 +14,6 @@ CREATE TABLE "favorites" (
 );
 
 DROP TABLE "favorites";
+
+INSERT INTO "favorites" ("url", "categoryId")
+VALUES ('www.google.com', 1);
--- src/components/GifList/GifList.jsx
+++ src/components/GifList/GifList.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 function GifList() {
+    const dispatch = useDispatch();
+    const gifList = useSelector(store => store.gifListReducer);
+
+    useEffect(() => {
+        dispatch ({
+            type: 'GET_GIF'
+        })
+    }, []);
+
+
     return (
         <h1>Here are some GIFs</h1>
     )
