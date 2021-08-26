4849686d53ea4f59c46dbfcde8b33ee488dc3936
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-08-26 14:34:31 -0500

    added search component

---
+++ src/components/SearchGif/SearchGif.jsx
@@ -0,0 +1,17 @@
+import{useState} from 'react'
+function SearchGif () {
+    let [search, setSearch] = useState();
+    const searchGiPhy = () => {
+        console.log('On search for giphy')
+    }
+    return(
+        <>
+        <h1>Search for Giphy</h1>
+        <input type="text" placeholder="search" onChange={(event) => setSearch(event.target.value)}
+        />
+        <button onClick={searchGiPhy}></button>
+        </>
+    )
+}
+
+export default SearchGif
